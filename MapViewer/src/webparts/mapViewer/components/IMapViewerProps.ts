ef22cf19ae93ec57bf537006b0faec766190d346
Bob German <bgerman1@hotmail.com>
2020-09-17 16:17:29 -0400

    Generated web part project

---
+++ MapViewer/src/webparts/mapViewer/components/IMapViewerProps.ts
@@ -0,0 +1,3 @@
+export interface IMapViewerProps {
+  description: string;
+}
---
+++ MapViewer/src/webparts/mapViewer/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMapViewerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MapViewerWebPartStrings' {
+  const strings: IMapViewerWebPartStrings;
+  export = strings;
+}
